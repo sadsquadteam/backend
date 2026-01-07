31ba9d15951696bb1a82fa9fedc6a194f62816b1
Samin Akbari <saminakbari.e@gmail.com>
2026-01-07 21:40:07 +0330

    feat: Create comment serializer and view

---
+++ interaction/serializers/comment.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from interaction.models.comment import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.id")
+
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "user",
+            "item",
+            "text",
+            "parent",
+            "created_at",
+        ]
+        read_only_fields = [
+            "created_at",
+        ]
---
+++ interaction/views/comment.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from interaction.models.comment import Comment
+from interaction.serializers.comment import CommentSerializer
+
+
+class CommentModelViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        if self.action in ['list', 'retrieve']:
+            return Comment.objects.all()
+        else:
+            return self.request.user.comments.all()
